32aa264c87cc5cfc76a3fc231a31c20e30bed820
juliosneto <juliosnneto4@gmail.com>
2021-09-11 13:09:00 -0300

    aula-4(4)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
-require("express")()
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
 .get("/", (req, res) => {
-    return res.send(__dirname + "/views/index.html")
+    return res.sendFile(__dirname + "/views/index.html")
+})
+
+.get("/study", (req, res) => {
+    return res.sendFile(__dirname + "/views/study.html")
+})
+
+.get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/views/give-classes.html")
 })
 .listen(5500)
